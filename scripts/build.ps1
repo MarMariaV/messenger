86554c1cc68d3c22f38016deac5ffaa3469ff1cc
MarMariaV <marmaria.v@gmail.com>
2023-12-21 14:14:15 +0300

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -5,19 +5,11 @@ param
     $Prefix = "/usr"
 )
 
-if ($IsWindows) {
-    dist/scripts/vcvars.ps1
-}
-
 if ((qmake --version -split '\n')[1][17] -eq '6') {
     qmake QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" $args[0] PREFIX=$Prefix DEFINES+="$env:nightlyDefines"
 } else {
     qmake $args[0] PREFIX=$Prefix DEFINES+="$env:nightlyDefines"
 }
 
+make
 
-if ($IsWindows) {
-    nmake
-} else {
-    make
-}
